156fd84b6169b9917127bdf1d979b59ecd8d89db
Brayan Huaman <brayanyhv@gmail.com>
2019-11-04 17:46:09 -0500

    correcciones

--- app/src/main/java/pe/com/pedidosya/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/pe/com/pedidosya/ui/notifications/NotificationsFragment.java
@@ -5,13 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -22,7 +18,6 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
@@ -36,16 +31,9 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
 import pe.com.pedidosya.R;
-import pe.com.pedidosya.adapter.CategoriaAdapter;
 import pe.com.pedidosya.adapter.EmpresaAdapter;
 import pe.com.pedidosya.api.Api;
-import pe.com.pedidosya.api.ApiDos;
-import pe.com.pedidosya.api.NetworkClient;
-import pe.com.pedidosya.beans.Categoria;
-import pe.com.pedidosya.beans.Data;
 import pe.com.pedidosya.beans.Empresa;
 import pe.com.pedidosya.beans.RepuestaEmpresa;
 import retrofit2.Call;
@@ -110,57 +98,5 @@ public class NotificationsFragment extends Fragment {
         });
     }
 
-    private void wsListarEmpresas(){
-        RequestQueue requestQueue = Volley.newRequestQueue(getActivity(), new HurlStack());
 
-        JSONObject jsonObject=new JSONObject();
-        try {
-            jsonObject.put("token",getActivity().getString(R.string.tokenApi));
-            jsonObject.put("codEmpresa","-");
-            jsonObject.put("tipo","-1");
-            jsonObject.put("codCategoria","-1");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        JsonObjectRequest jsonObject_Categoria=new JsonObjectRequest(Request.Method.POST, url, jsonObject, new com.android.volley.Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-
-                try {
-                    JSONArray jsonArray= response.getJSONArray("respuesta");
-                    if (jsonArray != null && jsonArray.length() > 0){
-                        empresaList.clear();
-                        for (int i = 0; i <jsonArray.length() ; i++) {
-                            JSONObject jsonObject = jsonArray.getJSONObject(i);
-                            Empresa categoria =new Empresa(
-
-                                    jsonObject.getString("cPerCodigo"),
-                                    jsonObject.getString("nomEmpAliada"),
-                                    jsonObject.getString("imagen"),
-                                    jsonObject.getString("banner"),
-                                    jsonObject.getString("direccion"),
-                                    jsonObject.getString("latitud"),
-                                    jsonObject.getString("longitud"),
-                                    jsonObject.getString("nAliTipo"),
-                                    jsonObject.getString("precioMinimo"),
-                                    jsonObject.getString("tipoEntrega"),
-                                    jsonObject.getString("nCantDiasReserva")
-                            );
-                            empresaList.add(categoria);
-                            rvCategoria.setAdapter(new EmpresaAdapter(getActivity(),empresaList));
-                        }
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        }, new com.android.volley.Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-            }
-        });
-
-        requestQueue.add(jsonObject_Categoria);
-    }
 }
